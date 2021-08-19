da26cdf19f44c36b11aa30acbffda73e35ef4e2f
hanhbyte <hell.boyx229@gmail.com>
2021-08-19 14:53:21 +0700

    up

---
+++ src/com/company/streamApi/functions/City.java
@@ -0,0 +1,19 @@
+package com.company.streamApi.functions;
+
+public class City {
+    private double longitude;
+    private double latitude;
+
+    public double getLogitude() {
+        return longitude;
+    }
+    public void setLogitude(double longitude) {
+        this.longitude = longitude;
+    }
+    public double getLatitude() {
+        return latitude;
+    }
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+}
